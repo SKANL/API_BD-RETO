b299b7723a124b906bc15aa6a482376c6440c30f
SKANL <anguaskujosegaspar@gmail.com>
2025-07-20 13:08:38 -0600

    Fix: use FindAsync in GenericRepository.GetByIdAsync

--- ApiBD.Infrastructure/Repositories/GenericRepository.cs
+++ ApiBD.Infrastructure/Repositories/GenericRepository.cs
@@ -16,7 +16,7 @@ namespace ApiBD.Infrastructure.Repositories
             _dbSet = _context.Set<T>();
         }
         public async Task<IEnumerable<T>> GetAllAsync() => await _dbSet.AsNoTracking().ToListAsync();
-        public async Task<T?> GetByIdAsync(object id) => await _dbSet.AsNoTracking().FirstOrDefaultAsync(e => EF.Property<object>(e, "Id") == id);
+        public async Task<T?> GetByIdAsync(object id) => await _dbSet.FindAsync(id);
         public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
         public void Update(T entity) => _dbSet.Update(entity);
         public void Delete(T entity) => _dbSet.Remove(entity);
