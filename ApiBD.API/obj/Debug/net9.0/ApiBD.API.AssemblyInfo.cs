a9c6c47db2f103b885d4c06563aa681a6964057b
SKANL <anguaskujosegaspar@gmail.com>
2025-07-20 17:09:48 -0600

    Actualización de código y configuración

--- ApiBD.API/obj/Debug/net9.0/ApiBD.API.AssemblyInfo.cs
+++ ApiBD.API/obj/Debug/net9.0/ApiBD.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiBD.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eccc7a25bd9ba3d660903c509bd4a697951da54c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9a1a8a545ec0d1ede3beeb745498d0ab289cddf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiBD.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiBD.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
